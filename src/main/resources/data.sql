9a8ee8d0515b6c8315709b8eb38277efe977c747
abdelilah dehaoui <abdelilah0dehaoui@outlook.fr>
2020-01-02 02:22:32 +0100

    add hiberntae diagramme

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into users (username, password, enabled)
-values ('admin', 'admin', true);
-insert into users (username, password, enabled)
-values ('user', 'user', true);
-
-insert into authorities(username, authority)
-    value ('admin', 'ROLE_ADMIN');
-insert into authorities(username, authority)
-    value ('user', 'ROLE_USER')
