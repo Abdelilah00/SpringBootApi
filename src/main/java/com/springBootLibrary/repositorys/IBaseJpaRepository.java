95c9bd28bcc82f25907577b26a1b44223adea2a2
abdelilah dehaoui <abdelilah0dehaoui@outlook.fr>
2020-01-23 18:15:04 +0100

    Dto => Entity  Done 100%

--- src/main/java/com/springBootLibrary/repositorys/IBaseJpaRepository.java
+++ src/main/java/com/springBootLibrary/repositorys/IBaseJpaRepository.java
@@ -2,11 +2,10 @@ package com.springBootLibrary.repositorys;
 
 import com.springBootLibrary.entitys.IdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 
-@Repository
+//@Repository
 public interface IBaseJpaRepository<TEntity extends IdEntity> extends JpaRepository<TEntity, Serializable> {
 
 }
